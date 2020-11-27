17421bf26bed4aa74f2890488a7ed19c218ebfd5
MujicaJohn <73504782+MujicaJohn@users.noreply.github.com>
2020-11-27 10:15:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,47 @@
-setInterval(showTime, 1000); 
-function showTime() { 
-    let time = new Date(); 
-    let hour = time.getHours(); 
-    let min = time.getMinutes(); 
-    let sec = time.getSeconds(); 
-    am_pm = "AM"; 
-  
-    if (hour > 12) { 
-        hour -= 12; 
-        am_pm = "PM"; 
-    } 
-    if (hour == 0) { 
-        hr = 12; 
-        am_pm = "AM"; 
-    } 
-  
-    hour = hour < 10 ? "0" + hour : hour; 
-    min = min < 10 ? "0" + min : min; 
-    sec = sec < 10 ? "0" + sec : sec; 
-  
-    let currentTime = hour + ":" 
-            + min + ":" + sec + am_pm; 
-  
-    document.getElementById("clock") 
-            .innerHTML = currentTime; 
-} 
-showTime(); 
+<script>
+      var myVar = setInterval(myTimer, 1000);
+      var hours = ["12",
+      "01","02","03","04","05","06","07","08","09","10","11","12",
+      "01","02","03","04","05","06","07","08","09","10","11"];
+      var months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ];
+      function myTimer() {
+        var date = new Date();
+        document.getElementById("date").innerHTML =
+          date.getDate() +
+          "-" +
+          months[date.getMonth()] +
+          "-" +
+          date.getFullYear();
+          document.getElementById("hours").innerHTML = hours[date.getHours()];
+        document.getElementById("minutes").innerHTML = date.getMinutes()<10?"0"+date.getMinutes() :date.getMinutes();
+        document.getElementById("seconds").innerHTML = date.getSeconds()<10?"0"+date.getSeconds() :date.getSeconds();
+        document.getElementById("ampm").innerHTML = date.getHours()<12?"AM":"PM";
+        
+        if (document.getElementById("colon1").innerHTML.includes(":"))
+        {
+        
+        document.getElementById("colon2").innerHTML = "";
+        document.getElementById("colon1").innerHTML = "";
+        }
+        else 
+        {
+            
+            document.getElementById("colon2").innerHTML = ":";
+        document.getElementById("colon1").innerHTML = ":";
+        }
+        
+      }
+    </script>
